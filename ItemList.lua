ad1b1f223b2125e7c179cad944a98535a6f28c0e
roflgenius <49673587+roflgenius@users.noreply.github.com>
2021-11-19 17:15:41 -0500

    Add files via upload

---
+++ ItemList.lua
@@ -0,0 +1,19 @@
+local Items = {}
+--[[
+Table that contains information about all currencies currently available.
+]]
+--!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--Be careful editing beyond this point
+--!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Items = {
+	Coin = {
+		["Name"] = "Coin";
+		["DropRate"] = 100;
+	};
+	Gem = {
+		["Name"] = "Gem";
+		["DropRate"] = 10;
+	}
+}
+
+return Items
